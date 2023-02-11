58877c2041d9396bf38c4001a1c935c6fbe7af8a
ELAVARASAN M <elavarasan.it20@bitsathy.ac.in>
2023-02-11 15:22:55 +0530

    Header updated

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -2,14 +2,14 @@
   <div>
     <nav class="navbar navbar-expand-lg fixed-top" style="background-color:#28364d;">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="@/assets/quinbaylogo.png" width="150vw" height="50vh" alt="logo"></a>
+        <a class="navbar-brand" href=""><img src="@/assets/quinbaylogo.png" width="150vw" height="50vh" alt="logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item active text-white">
-              <a class="nav-link text-light" href="#">{{ user.username }} <span class="sr-only">({{user.about}})</span></a>
+              <a class="nav-link text-light f-bold">Group Chat</a>
             </li>
             <li class="nav-item dropdown ">
               <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -25,22 +25,26 @@
           </ul>
         </div>
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src="user-avatar.png" alt="user avatar" class="img-fluid rounded-circle">
+
+          <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle text-light" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              
+            <span class="text-light">{{user.username}} </span>
+            <img src="@/assets/avatar.png" width="40vw" height="40vh" alt="user avatar" class="img-fluid rounded-circle ">
             </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            <div class="dropdown-menu" aria-labelledby="userDropdown">
               <a class="dropdown-item" href="#">Profile</a>
               <a class="dropdown-item" href="#">Sign Out</a>
             </div>
           </li>
         </ul>
+        
       </div>
     </nav>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
