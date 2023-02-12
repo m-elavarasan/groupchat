20d681e045bb3421f099cebc8bfdcb85a1e713ea
ELAVARASAN M <elavarasan.it20@bitsathy.ac.in>
2023-02-12 08:25:20 +0530

    apiservice update

--- src/apiservice/userGroups.js
+++ src/apiservice/userGroups.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-
+const API = axios.create({
+  baseURL: 'http://localhost:8085'
+})
 export default {
   async fetchGroups(mobile) {
     try {
-      const response = await axios.get(`http://localhost:8085/showGroup?mobilenum=${mobile}`);
+      const response = await API.get(`/showGroup?mobilenum=${mobile}`);
       return response.data;
     } catch (error) {
       console.error(error);
